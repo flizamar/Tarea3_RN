1a619c21d1b05135dfb79070f02b1a5f2d7bfe45
Felipe Lizama <felipealonsolr@gmail.com>
2019-01-04 22:56:55 -0300

    Agregado lo básico

---
+++ geneticProgramming.py
@@ -0,0 +1,19 @@
+import numpy as np
+import math
+import random
+
+ops = ["+", "-", "*", "/", "max", "min"]
+
+class Nodo():
+  def __init__(self, val):
+    self.val = val
+    self.izq = None
+    self.der = None
+  def isLeaf():
+    return self.izq != None or self.der != None
+
+class Arbol():
+  def __init__(self, raiz):
+    self.raiz = raiz
+  def subTreeCrossOver(arbol):
+    return
